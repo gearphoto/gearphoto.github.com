24fd4f75271cdced853fa4fb21b9efc5ea669085
gearphoto <gearphotoapp@gmail.com>
2016-12-01 17:41:15 +0500

    finish

---
+++ index.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('.layer').addClass('scale');
+	}, 1000);
+
+	// }, 3000)
+	var options = { videoId: 'SdMqV4h9kVg', start: 3 };
+	$('.layer').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+	// metU1j3ixCw
+
+});
